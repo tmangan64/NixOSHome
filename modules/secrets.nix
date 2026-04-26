83160db8be1fd50944b7f5ab71f000da3a0f64a2
Teague M <65279644+tmangan64@users.noreply.github.com>
2026-04-27 00:55:00 +0100

    Ok it might actually be fixed

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -5,10 +5,9 @@
     defaultSopsFile = ../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
 
-    # Decrypts using the host's ed25519 SSH key, converted to age at activation.
+    # Decrypt using the host's SSH key converted to age format.
+    # The SSH host key must be added to .sops.yaml before deploy.
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-    age.keyFile = "/var/lib/sops-nix/key.txt";
-    age.generateKey = true;
 
     secrets = {
       "admin/password_hash" = {
