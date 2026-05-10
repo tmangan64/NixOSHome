c4fbc29f2be549a36151a6462e43e13b5b37f0dc
Teague M <65279644+tmangan64@users.noreply.github.com>
2026-05-10 11:24:54 +0100

    Created config

---
+++ options/options.nix
@@ -0,0 +1,47 @@
+{
+  # Host Configuration
+  hostname = "testhostname";
+  domain = "domain";
+
+  # Locale & Time
+  timeZone = "Europe/London";
+  locale = "en_GB.UTF-8";
+  keyMap = "uk";
+  phoneRegion = "GB";
+
+  # Network
+  interface = "enp3s0";
+  ipAddress = "192.168.0.55";
+  prefixLength = 24;
+  gateway = "192.168.0.1";
+  nameservers = [ "127.0.0.1" "1.1.1.1" "9.9.9.9" ];
+
+  # SSH
+  sshPort = 33;
+  sshKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHEr9l0xPvco+x1zz2X5skaIwpjtI0+QGOELm/KtV5d kiroshi" ];
+
+  # Admin User
+  adminUser = "admin";
+
+  # Services - Nextcloud
+  nextcloudDomain = "cloud.home";
+  nextcloudDataDir = "/srv/data/nextcloud";
+  nextcloudMaxUpload = "16G";
+  nextcloudPort = 8080;
+
+  # Services - AdGuard / DNS
+  adguardPort = 3000;
+  dnsPort = 53;
+  dnsDomain = "dns.home";
+  upstreamDns = [ "https://dns.cloudflare.com/dns-query" "https://dns.quad9.net/dns-query" ];
+  bootstrapDns = [ "1.1.1.1" "9.9.9.9" ];
+
+  # Fail2ban
+  fail2banMaxRetry = 5;
+  fail2banBantime = "1h";
+
+  # Disks
+  osDisk = "/dev/nvme0n1";
+  dataDisk = "/dev/sda1";
+  dataMount = "/srv/data";
+}
